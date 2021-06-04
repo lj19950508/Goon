f6656c969addf0be877d4ffb3c5a0ea4779b2678
linjie2 <linjie2@linewell.com>
2021-06-04 15:49:09 +0800

    修改抽代码

--- interfaces/web/src/main/resources/templates/modules/scrum/gencode/entity/entityForm.js
+++ interfaces/web/src/main/resources/templates/modules/scrum/gencode/entity/entityForm.js
@@ -38,38 +38,74 @@ var pageFormObject={
                 },
                 {
                     //下拉
+                    width:100,
                     field: 'formType',
                     title: '表单组件',
+                    formatter:function(value, row, index){
+                        return '<select  data-width="auto" name="name" class="form-control" data-style="btn-default" data-live-search="true">'+
+                                    '<option value="1" label="1">下拉框</option>'+
+                                    '<option value="2" label="2">文件选择器</option>'+
+                               '</select>'
+                    }
                 },
-
                 {
                     //下拉
+                    width:100,
                     field: 'itemType',
-                    title: 'JAVA类型',
+                    title: 'java类型',
+                    formatter:function(value, row, index){
+                        return '<select  data-width="auto"   name="name" class="form-control" data-style="btn-default" data-live-search="true">'+
+                                    '<option value="1" label="1">String</option>'+
+                                    '<option value="2" label="2">Integer</option>'+
+                                    '<option value="2" label="2">Area</option>'+
+                               '</select>'
+                    }
                 },
                 {
                     //下拉
+                    width:100,
                     field: 'sqlType',
                     title: 'sql类型',
+                    formatter:function(value, row, index){
+                        return '<select  data-width="auto"   name="name" class="form-control" data-style="btn-default" data-live-search="true">'+
+                            '<option value="1" label="1">varchar</option>'+
+                            '<option value="2" label="2">char</option>'+
+                            '<option value="2" label="2">bigInt</option>'+
+                            '</select>'
+                    }
                 },
                 {
                     //输入
                     field: 'sqlLength',
                     title: 'sql长度',
-                    width:100,
+                    width:150,
                     formatter: function (value, row, index) {
                         return '<input type="text"  value="' + value + '" class="form-control" name="itemDesc" placeholder="字段长度">'
                     }
                 },
                 {
                     //下拉
+                    width:100,
                     field: 'queryType',
-                    title: '查询组件类型',
+                    title: '查询组件',
+                    formatter:function(value, row, index){
+                        return '<select  data-width="auto"   name="name" class="form-control" data-style="btn-default" data-live-search="true">'+
+                            '<option value="1" label="1">下拉框</option>'+
+                            '<option value="2" label="2">文件选择器</option>'+
+                            '</select>'
+                    }
                 },
                 {
                     //下拉
+                    width:100,
                     field: 'queryExp',
                     title: '查询表达式',
+                    formatter:function(value, row, index){
+                        return  '<select   data-width="auto"  name="name" class="form-control" data-style="btn-default" data-live-search="true">'+
+                                    '<option value="1" label="1">不相等</option>'+
+                                    '<option value="2" label="2">相等</option>'+
+                                '</select>'
+                    }
 
                 },
                 {
@@ -83,41 +119,52 @@ var pageFormObject={
                 },
                 {
                     //toggle
+                    width:100,
                     field: 'isMust',
                     title: '必填',
                     formatter:function(value, row, index){
                         return '<div class="custom-control custom-switch">' +
                             '       <input type="checkbox" class="custom-control-input" id="customSwitch1">' +
-                            '       <label class="custom-control-label" for="customSwitch1">label</label>' +
+                            '       <label class="custom-control-label" for="customSwitch1"></label>' +
                             '   </div>'
                     }
                 },
                 {
                     //toggle
+                    width:100,
                     field: 'isUnique',
                     title: '唯一',
                     formatter:function(value, row, index){
                         return '<div class="custom-control custom-switch">' +
                             '       <input type="checkbox" class="custom-control-input" id="customSwitch1">' +
-                            '       <label class="custom-control-label" for="customSwitch1">label</label>' +
+                            '       <label class="custom-control-label" for="customSwitch1"></label>' +
                             '   </div>'
                     }
                 },
                 {
                     //toggle
+                    width:100,
                     field: 'isSort',
                     title: '排序',
                     formatter:function(value, row, index){
                         return '<div class="custom-control custom-switch">' +
                             '       <input type="checkbox" class="custom-control-input" id="customSwitch1">' +
-                            '       <label class="custom-control-label" for="customSwitch1">label</label>' +
+                            '       <label class="custom-control-label" for="customSwitch1"></label>' +
                             '   </div>'
                     }
                 },
                 {
                     //下拉
+                    width:150,
                     field: 'dictId',
                     title: '关联字典',
+                    formatter:function(value, row, index){
+
+                        return '<select  data-width="auto"   name="name" class="selectpicker item-select" data-style="btn-default" data-live-search="true">'+
+                            '<option value="1" label="1">非常厉害的字段</option>'+
+                            '<option value="2" label="2">2</option>'+
+                            '</select>'
+                    }
                 },
                 {
                     field: 'operate',
@@ -148,14 +195,24 @@ var pageFormObject={
     },
     getData:function(){
         if(true){
-            this.tableRef.bootstrapTable('append', [{itemName:'',itemDesc:'',formType:'',itemType:'',sqlType:'',sqlLength:'',queryType:'',queryExp:'',listLength:'',isMust:0,isUnique:0,isSort:0,dictId:''}]);
+            this.appendNewData()
         }else{
 
         }
         //
     },
     appendNewData(){
+        //保存已有数据
+        this.
         this.tableRef.bootstrapTable('append', [{itemName:'',itemDesc:'',formType:'',itemType:'',sqlType:'',sqlLength:'',queryType:'',queryExp:'',listLength:'',isMust:0,isUnique:0,isSort:0,dictId:''}]);
+        // this.tableRef.bootstrapTable('insertRow',{index:1,row:{itemName:'',itemDesc:'',formType:'',itemType:'',sqlType:'',sqlLength:'',queryType:'',queryExp:'',listLength:'',isMust:0,isUnique:0,isSort:0,dictId:''}} );
+        //bug1.刷新后值丢失
+        //bug2.table列宽没有适应
+        $(".item-select").selectpicker('show')
+    },
+    //保存临时数据
+    tableTempSave(){
+
     },
     del:function(row,index){
         let totals = this.tableRef.bootstrapTable('getData').length;
